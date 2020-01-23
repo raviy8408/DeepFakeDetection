dc3703de76af651998f55205b78ee2d9be5b8865
Ravi Yadav <raviy8408@gmail.com>
2020-01-23 12:46:38 +0530

    mtcnn face detection added

--- FaceExtractMTCNN.py
+++ FaceExtractMTCNN.py
@@ -4,11 +4,11 @@ from sklearn.model_selection import train_test_split
 import cv2
 import math
 from tqdm import tqdm
+from mtcnn.mtcnn import MTCNN
 
 
-# importing face cascade
-face_cascade = cv2.CascadeClassifier(r'C:\Users\Ravi Yadav\PycharmProjects\OpenCVPrac\cascades\data'
-                                     r'\haarcascade_frontalface_default.xml')
+# importing face detector
+detector = MTCNN()
 
 # create train and test video split
 pd_dt = pd.read_json(r'C:\Ravi\files\deepfake-detection-challenge\train_sample_videos\metadata.json').T.reset_index()
@@ -34,13 +34,16 @@ for i in tqdm(range(X_train.shape[0])):
             break
         if frameId % math.floor(frameRate) == 0:
             # storing the frames in a new folder named train_1
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.25, minNeighbors=6, minSize=(100,100))
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
-                # Save faces in a folder
-                roi_color = frame[y:y + h, x:x + w]
-                count += 1
-                cv2.imwrite('C:/Ravi/files/deepfake-detection-challenge/train/' + videoFile + "_"
-                            + "frame%d.jpg" % count, roi_color)
+            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = detector.detect_faces(frame)
+            # print(faces)
+            # print([faces[0]['box']])
+            if len(faces):
+                for (x, y, w, h) in [faces[0]['box']]:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                    # Save faces in a folder
+                    roi_color = frame[y:y + h, x:x + w]
+                    count += 1
+                    cv2.imwrite('C:/Ravi/files/deepfake-detection-challenge/train_mtcnn/' + videoFile + "_"
+                                + "frame%d.jpg" % count, roi_color)
     cap.release()
